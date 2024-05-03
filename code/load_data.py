6cce18102fb74a57b7afcb706e1e129d94d0269e
Kai Lascheit <kai.lascheit.000@gmail.com>
2024-05-03 14:57:17 +0200

    minor adjustments to data loading

--- code/load_data.py
+++ code/load_data.py
@@ -1,26 +1,27 @@
 import pandas as pd
+import pdb
 
 
-def load_data(train_path_neg, train_path_pos, test_path, val_split=0.1):
-    train_neg = pd.read_fwf(train_path_neg, header=None, names=["tweet"])
-    train_neg["label"] = 0
-    train_neg = train_neg[:int(val_split * len(train_neg))]
-    val_neg = train_neg[int(val_split * len(train_neg)):]
+def load_data(train_path_neg, train_path_pos, test_path, val_split=0.9, frac=1):
+    data_neg = pd.read_fwf(train_path_neg, header=None, names=["tweet"])
+    data_neg["label"] = 0
+    train_neg = data_neg[:int(val_split * len(data_neg))]
+    val_neg = data_neg[int(val_split * len(data_neg)):]
 
-    train_pos = pd.read_fwf(train_path_pos, header=None, names=["tweet"])
-    train_pos["label"] = 1
-    train_pos = train_pos[:int(val_split * len(train_pos))]
-    val_pos = train_pos[int(val_split * len(train_pos)):]
+    data_pos = pd.read_fwf(train_path_pos, header=None, names=["tweet"])
+    data_pos["label"] = 1
+    train_pos = data_pos[:int(val_split * len(data_pos))]
+    val_pos = data_pos[int(val_split * len(data_pos)):]
 
-    train = pd.concat([train_neg, train_pos], ignore_index=True).sample(frac=1).reset_index(drop=True)
+    train = pd.concat([train_neg, train_pos], ignore_index=True).sample(frac=frac).reset_index(drop=True)
     X_train = train["tweet"].squeeze()
     y_train = train["label"].squeeze()
 
-    val = pd.concat([val_neg, val_pos], ignore_index=True).sample(frac=1).reset_index(drop=True)
+    val = pd.concat([val_neg, val_pos], ignore_index=True).sample(frac=frac).reset_index(drop=True)
     X_val = val["tweet"].squeeze()
     y_val = val["label"].squeeze()
 
-    X_test = pd.read_fwf(test_path, header=None, names=["tweet"]).squeeze()
+    X_test = pd.read_fwf(test_path, header=None, names=["tweet"]).squeeze()        
 
     return X_train, y_train, X_val, y_val, X_test
 
