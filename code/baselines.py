fb4a250e50647ecef8a0ccb0d95cf2f4b68d8b80
jlohunecke <jacobhunecke@gmai.com>
2024-05-03 09:42:33 +0200

    implemented a couple of baselines in notebook

--- code/baselines.py
+++ code/baselines.py
@@ -1,11 +1,13 @@
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
-from sklearn.preprocessing import MinMaxScaler
+import torch.nn as nn
+
+
+
 
 class NaiveBayes:
     def __init__(self):
-        self.model = Pipeline([('Normalizing',MinMaxScaler()),('MultinomialNB',MultinomialNB())])
+        self.model = MultinomialNB()
 
     def fit(self, X_train, y_train):
         self.model.fit(X_train, y_train)
@@ -16,3 +18,7 @@ class NaiveBayes:
         print(confusion_matrix(y_val, y_pred))
         print(classification_report(y_val, y_pred))
         print(accuracy_score(y_val, y_pred))
+
+
+
+
--- code/embed.py
+++ code/embed.py
@@ -1,35 +1,47 @@
 import numpy as np
 from gensim.models import Word2Vec
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 
 class Embedder:
 
     def __init__(self):
 
-        self.pipeline = self.pipeline = Pipeline([
-            ('bow', CountVectorizer()),  # strings to token integer counts
-            ('tfidf', TfidfTransformer()),  # integer counts to weighted TF-IDF scores
-        ])
+        self.model = None
+
         pass
 
-    def fit(self, X_train_tokens):
-        X_train_tokens = X_train_tokens.astype(str)
-        self.pipeline.fit(X_train_tokens)
-        return self.pipeline
+    def fit(self, X_tokens):
 
-    def get_tweet_vector(self, tweet):
-        vector_size = self.embedder.vector_size
-        token_embeddings = []
+        # Train word2vec model
+        self.model = Word2Vec(X_tokens, vector_size=100, window=5, min_count=1, workers=4)
 
-        for token in tweet:
+        pass
+
+    def embed(self, X_tokens):
+
+        preprocessed_sentences = [' '.join(tokens) for tokens in X_tokens]
+
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(preprocessed_sentences)
+        tfidf_array = tfidf_matrix.toarray()
+
+        # Get word embeddings
+        word_embeddings = []
+        for word in vectorizer.get_feature_names_out():
             try:
-                token_embeddings.append(self.embedder.wv[token])
-            except KeyError:
+                word_embeddings.append(self.model.wv[word])
+            except:
                 pass
 
-        if token_embeddings:
-            return np.mean(token_embeddings, axis=0)
-        else:
-            return np.zeros(vector_size)
+        sentence_embeddings = []
+        for row in tfidf_array:
+            sentence_embedding = np.dot(row, word_embeddings)
+            sentence_embeddings.append(sentence_embedding)
+
+        sentence_embeddings = np.array(sentence_embeddings)
+        #norms = np.linalg.norm(sentence_embeddings, axis=1, keepdims=True)
+        #normalized_embeddings = sentence_embeddings / norms
+
+        return sentence_embeddings
