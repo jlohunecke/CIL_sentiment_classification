3dc273912b90cd3280e4683356e14530a2716571
Kai Lascheit <kai.lascheit.000@gmail.com>
2024-05-09 14:32:50 +0200

    reimplement gru with pytorch

--- code/gru.py
+++ code/gru.py
@@ -1,58 +1,103 @@
 from os import getcwd
-import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import save_model, load_model
-from sklearn.metrics import accuracy_score, classification_report
+import torch
+import torch.nn as nn
+from torchtext.data import get_tokenizer
+from collections import Counter
+from torch.nn.utils.rnn import pad_sequence
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
 
 from load_data import load_data
-from preprocess import preprocess
 
-RETRAIN = False
-model_path = getcwd() + "/../models/"
+RETRAIN = True
+MAX_SEQUENCE_LENGTH = 40
+MAX_VOCAB_SIZE = 20000
+GRU_UNITS = 64
+NUM_EPOCHS = 10
+BATCH_SIZE = 32
+MODEL_SAVE_PATH = getcwd() + "/models/model_weights.pth"
 
 # load data
 train_path_neg = getcwd() + "/twitter-datasets/train_neg.txt"
 train_path_pos = getcwd() + "/twitter-datasets/train_pos.txt"
 test_path = getcwd() + "/twitter-datasets/test_data.txt"
-X_train, y_train, X_val, y_val, X_test = load_data(train_path_neg, train_path_pos, test_path, frac=0.2)
+X_train, y_train, X_val, y_val, X_test = load_data(train_path_neg, train_path_pos, test_path, frac=1.0)
 
-# preprocess 
-max_sequence_length = 40
-max_vocab_size = 20000
-tokenizer = Tokenizer(num_words=max_vocab_size, oov_token='<UNK>')
-tokenizer.fit_on_texts(X_train)
-train_sequences = tokenizer.texts_to_sequences(X_train)
-train_inputs = pad_sequences(train_sequences, maxlen=max_sequence_length)
-tokenizer.fit_on_texts(X_val)
-val_sequences = tokenizer.texts_to_sequences(X_val)
-val_inputs = pad_sequences(val_sequences, maxlen=max_sequence_length)
+# preprocess
+def preprocess_for_rnn(X, y):
+    tokenizer = get_tokenizer("basic_english")
+    tokenized_sequences = X.apply(tokenizer)
+    all_tokens = [token for sublist in tokenized_sequences for token in sublist]
+    counter = Counter(all_tokens)
+    indexed_vocab = {elt[0] : idx for idx, elt in enumerate(counter.most_common(MAX_VOCAB_SIZE-1))}
+    indexed_data = [torch.tensor([indexed_vocab.get(token, MAX_VOCAB_SIZE-1) for token in sequence]) for sequence in tokenized_sequences]
+    padded_data = pad_sequence(indexed_data, batch_first=True)
+    labels = torch.Tensor(y)
+    return TensorDataset(padded_data[:, :MAX_SEQUENCE_LENGTH], labels)
 
-# train
+train_dataset = preprocess_for_rnn(X_train, y_train)
+val_dataset = preprocess_for_rnn(X_val, y_val)
+
+# define RNN
+class SentimentAnalysisRNN(nn.Module):
+    def __init__(self, max_vocab_size, embedding_dim, gru_units):
+        super(SentimentAnalysisRNN, self).__init__()        
+        self.embedding = nn.Embedding(max_vocab_size, embedding_dim)
+        self.gru = nn.GRU(embedding_dim, gru_units, dropout=0.2, batch_first=True, num_layers=3)
+        self.dropout = nn.Dropout(0.2)
+        self.fc = nn.Linear(gru_units, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        output, _ = self.gru(embedded)
+        output = self.dropout(output[:, -1, :])  # Taking the last hidden state
+        output = self.fc(output)
+        output = self.sigmoid(output)
+        return output.squeeze(1)  # Squeeze to remove the extra dimension
+    
+# train RNN
+print("Training started")
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+model = SentimentAnalysisRNN(MAX_VOCAB_SIZE, MAX_SEQUENCE_LENGTH, GRU_UNITS)
 if RETRAIN:
-    embedding_dim = 50
-    gru_units = 64
-    model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(input_dim=max_vocab_size, output_dim=embedding_dim, input_length=max_sequence_length),
-        tf.keras.layers.GRU(units=gru_units, dropout=0.2, recurrent_dropout=0.2),
-        tf.keras.layers.Dense(units=1, activation='sigmoid')
-    ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    model.summary()
-    batch_size = 32
-    epochs = 1
-    model.fit(train_inputs, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
-    save_model(model, model_path + "gru_model.h5")
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    model.train()  # Set the model to training mode
+    for epoch in range(NUM_EPOCHS):
+        # perform training
+        train_loss = 0.0
+        for inputs, labels in train_dataloader:
+            optimizer.zero_grad()  # Zero the gradients
+            outputs = model(inputs) # forward pass
+            loss = criterion(outputs, labels.float()) # compute loss
+            loss.backward() # Backward pass   
+            optimizer.step() # Update the parameters        
+            train_loss += loss.item()
+        train_loss /= len(train_dataloader)
+        # perform validation
+        model.eval()  # Set model to evaluation mode
+        val_loss = 0.0
+        val_correct = 0
+        val_total = 0
+        with torch.no_grad():  # Disable gradient computation for validation
+            for inputs, labels in val_dataloader:
+                outputs = model(inputs)
+                loss = criterion(outputs, labels.float())
+                val_loss += loss.item()
+                predicted = torch.round(outputs)
+                val_correct += (predicted == labels).sum().item()
+                val_total += labels.size(0)
+        val_loss /= len(val_dataloader)
+        val_accuracy = val_correct / val_total
+        print(f'Epoch {epoch+1}/{NUM_EPOCHS}, '
+            f'Train Loss: {train_loss:.4f}, '
+            f'Val Loss: {val_loss:.4f}, '
+            f'Val Accuracy: {val_accuracy:.2%}')
+    print("Training finished")
+    torch.save(model.state_dict(), MODEL_SAVE_PATH)
+    print("Model weights saved to:", MODEL_SAVE_PATH)
 else:
-    model = load_model(model_path + "gru_model.h5")
-
-# validate
-predictions = model.predict(val_inputs)
-binary_predictions = (predictions > 0.5).astype(int)
-accuracy = accuracy_score(y_val, binary_predictions)
-report = classification_report(y_val, binary_predictions)
-
-print("Accuracy:", accuracy)
-print("Classification Report:\n", report)
-
-print()
+    model.load_state_dict(torch.load(MODEL_SAVE_PATH))
+    print("Loaded pretrained weights from:", MODEL_SAVE_PATH)
